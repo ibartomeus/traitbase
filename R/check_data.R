59272806fa117bce00877950365608ad93baa52c
Nacho <nacho@air>
2016-05-06 14:12:19 +0200

    Check_data ready to be tested.

--- R/check_data.R
+++ R/check_data.R
@@ -5,18 +5,18 @@
 #' @description checks planed: fits the schema, taxonomy, units, geography, phenology
 #'  
 #' @param schema which schema is the data prepared for: Only "bee" available.
-#' @param data a dataset to check
+#' @param dat a dataset to check
 #' @param type specimens or observations data
 #' @param error Should give an error or just remove failing rows? default FALSE. 
 #' 
-#' @return a clean dataset of an error
+#' @return a clean dataset or an error
 #'
 #' @examples 
 #' #not run
 #' 
 #' data1 <- data.frame(id = NA,
 #' link_id = NA,
-#' Genus = NA,
+#' genus = NA,
 #' species = NA,
 #' sex = NA,
 #' trait_category = NA, 
@@ -27,11 +27,12 @@
 #' 
 #' data2 <- data.frame(id = NA,
 #' link_id = NA,
-#' Genus = NA,
+#' genus = NA,
 #' species = NA,
 #' sex = NA,
-#' host_genus = NA,
-#' host_species = NA,
+#' interaction_type = NA,
+#' partner_genus = NA,
+#' partner_species = NA,
 #' day = NA,
 #' month = NA,
 #' year = NA,
@@ -44,50 +45,56 @@
 #' taxonomist = NA)
 
 #' check_data(data1, type = "specimens")
-#' check_data(data2, type = "specimens")
+#' check_data(data2, type = "observations")
 #'  
 #' @export
-check_data <- function(data, schema = "bee",
+check_data <- function(dat, schema = "bee",
                        type = c("specimens", "observations"),
                        error = FALSE){
     if(schema != "bee") stop("only available for bees")
+    load("data/bee_schema.rda")
     schema <- bee_schema
-    #checks common for both types
-    if(length(unique(data$link_id)) != length(data$link_id)){
-        stop("link_id should be unique") # isue: can I make it unique, and if so, 
-        #can I force observations and specimens same way? 
+
+    #checks common things for both types
+    if(length(unique(dat$link_id)) != length(dat$link_id)){
+        stop("link_id should be unique") 
     }
-    if(is.na(data$Genus) | is.na(data$species)){
+    if(is.na(dat$genus) | is.na(dat$species)){
         stop("Genus and species can't be NA. Only fully identified species are accepted")
     }
-    data$Gen_sp <- paste(data$Genus, data$species)
-    taxas <- tax_name(query = data$Gen_sp, get = "genus", verbose = FALSE)
+    dat$gen_sp <- paste(data$genus, data$species)
+    taxas <- tax_name(query = dat$gen_sp, get = "genus", verbose = FALSE)
     if(length(which(is.na(taxas$genus))) > 0){
-        data2 <- data[-which(is.na(taxas$genus)),]
+        dat <- dat[-which(is.na(taxas$genus)),]
         warning("Species not in itis removed") #add error = TRUE and id of rows removed
     }
-    if(!data$sex %in% c("male", "female", "queen", "worker")){
-        stop("Sex should be one of 'male', 'female', 'queen', 'worker' or NA")
-    } #here should we use the schema?
+    if(!dat$sex %in% eval(parse(text = schema[which(schema$trait == "sex"), 
+                                              "test"]))){
+        stop("sex should be one of 'male', 'female', 'queen', or NA")
+    } #check NA's are accepted. put schema in the warning.
     #reference: linked to bibtext??? Not sure yet how to implement that.
     #credit: Anything goes?
+    
     #now check specimens
     if(type == "specimens"){
-        if(colnames(data) != colnames(specimens)[-"id"]){
+        load("data/bee_specimens.rda") #this may be lots of data... think better way?
+        #maybe just save a head() of each dataset? Is this lame?
+        specimens <- bee_specimens #fix that when more than one schema is available.
+        if(colnames(dat) != colnames(specimens)[-"id"]){
             stop("Column names should match specimens columnames: see ?specimens")
-            #ToDo: automatically add some columns filled with NA with warning
+            #ToDo: automatically add missing columns filled with NA with warning
         } 
         #tests for each column
-        if(!data$trait_category %in% schema$trait_category){
+        if(!dat$trait_category %in% schema$category){
             stop("trait_category should match schema (see ?schema). If you need a new category contact me")
         }
-        trait_schema <- paste(data$trait_category, data$trait, sep = "_")
-        if(!trait_schema %in% paste(schema$trait_category, schema$trait, sep = "_")){
-            stop("trait should match schema (see ?schema). If you need a new trait type contact me")
+        trait_schema <- paste(dat$category, dat$trait, sep = "_")
+        if(!trait_schema %in% paste(schema$category, schema$trait, sep = "_")){
+            stop("trait should match schema (see taxa_schema). If you need a new trait type contact me")
         }
         #value: 
         #list traits
-        traits <- unique(data$trait)
+        traits <- unique(dat$trait)
         #subset by trait
         for(i in 1:length(traits)){
             temp <- subset(data, trait == traits[i])
@@ -97,63 +104,69 @@ check_data <- function(data, schema = "bee",
             #schema_test <- "c('nest', 'nest_soil')"
             #check
             if(!temp %in% eval(parse(text=schema_test))){
-                stop("value not apropiate...")
+                stop("value not llowed by the schema. See taxa_schema") #better error message?
             }
         }
-        #check also which columns has NA's allowed or not... 
+        #check also which columns are allowed to be NA's or not... 
     }
     if(type == "specimens"){
-        #host_genus
-        taxas <- tax_name(query = data$host_genus, get = "genus", verbose = FALSE)
+        #check columns
+        #idem from above, but we should avoid load.
+        #partner_genus
+        taxas <- tax_name(query = dat$partner_genus, get = "genus", verbose = FALSE)
         if(length(which(is.na(taxas$genus))) > 0){
-            data2 <- data[-which(is.na(taxas$genus)),]
-            warning("Genus not in itis removed") #add error = TRUE and id of rows removed
+            dat[which(is.na(taxas$genus)),] <- NA
+            warning("Genus not in itis. Made NA") #add error = TRUE and id of rows fixed
         } #check ITITS is the palce for plants. I know it is the best for bees, but...
-        #host_species
-        if(!is.na(data$host_species)){
-            data$Host_sp <- paste(data$host_genus, data$host_species)
-            taxas <- tax_name(query = data$Host_sp, get = "genus", verbose = FALSE)
+        #partner_species
+        if(!is.na(dat$partner_species)){
+            dat$partner_sp <- paste(dat$partner_genus, dat$partner_species)
+            taxas <- tax_name(query = data$partner_sp, get = "genus", verbose = FALSE)
             if(length(which(is.na(taxas$genus))) > 0){
-                data2 <- data[-which(is.na(taxas$genus)),] #better only remove species (= NA), not genus, then.
-                warning("Species not in itis removed") #add error = TRUE and id of rows removed
-            } #check ITITS is the palce for plants. I know it is the best for bees, but...
+                dat[which(is.na(taxas$genus)),] <- NA 
+                warning("species not in itis, turned to NA") #add error = TRUE and id of rows fixed
+            } 
         }
         #day
-        if(!data$day %in% c(1:31)){
+        if(!dat$day %in% c(1:31)){
             stop("months should have up to 31 days only") #I am not cheacking by month...
         } #I should indicate where it fails (along all the script)
         #month
-        if(!data$month %in% c(1:12)){
+        if(!dat$month %in% c(1:12)){
             stop("months should be numerated 1 to 12") 
         }
         #year
-        if(!data$year %in% c(1700:3000)){
+        if(!dat$year %in% c(1700:3000)){
             stop("year should be four digits and > 1700") 
         }
         #date
-        if(as.POSIXct(paste(data$year, data$day, data$month, sep = "-")) 
+        if(as.POSIXct(paste(dat$year, dat$day, dat$month, sep = "-")) 
                       > as.POSIXct(Sys.Date())){
-            stop("Collection date can't be on the futuere")
+            stop("Collection date can't be on the future")
         }
         #country: 
-        if(!data$contry %in% countrycode_data$country.name){
-            stop("country not recognized, see ?countrycode_data for a list")
-        } #we can try the regex match and fix here on the fly!
+        if(!dat$contry %in% countrycode_data$country.name){
+            stop("country not recognized, see ?countrycode_data for a list of accepted names")
+        } #we can try the regex to match and fix coomon mistakes here on the fly!
         #location: any string goes
         #lat:
-        if(data$lat %in% c(-85:85)){
+        if(dat$lat %in% c(-85:85)){
             stop("latitude should be between -85 and 85")
         }
         #long
-        if(data$lat %in% c(-180:180)){
+        if(dat$lat %in% c(-180:180)){
             stop("longitude should be between -180 and 180")
         }
         #accurancy
-        if(num.decimals(data$lat) | num.decimals(data$long) < 2){
-            warning("longitude and latitude have very low resolution")
+        if(num.decimals(dat$lat) | num.decimals(dat$long) < 2){
+            warning("longitude and latitude have very low resolution.")
         }
         #check lat long not in the see & in the targeted country...
-        
+        match_country <- coords2country(dat$lat, dat$long)
+        discrepancies <- which(dat$country != match_country)
+        if(length(discrpeancies) > 0){
+            warning("Some points fall on a different country than you mention or on the sea") #indicate which
+        } #what to do with US states?
         #collector: any string goes
         #taxonomist: any string goes
     }
--- process.R
+++ process.R
@@ -3,8 +3,10 @@ library(roxygen2)
 library(knitr)
 library(taxize) #add to dependencies
 library(countrycode) #idem
-library(plyr) #also
-library(git2r) #also
+library(plyr) #also (ot sure yet)
+library(git2r) #also (idem)
+library(sp) #add
+library(rworldmap) #add
 
 #add this as func
 num.decimals <- function(x) {
@@ -14,6 +16,26 @@ num.decimals <- function(x) {
     nchar(x)
 }
 
+#also this one
+
+# The single argument to this function, points, is a data.frame in which:
+#   - column 1 contains the longitude in degrees
+#   - column 2 contains the latitude in degrees
+coords2country = function(lat, long){  
+    countriesSP <- getMap(resolution='low')
+    #countriesSP <- getMap(resolution='high') #you could use high res map from rworldxtra if you were concerned about detail
+    # convert our list of points to a SpatialPoints object
+    # pointsSP = SpatialPoints(points, proj4string=CRS(" +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"))
+    #setting CRS directly to that from rworldmap
+    pointsSP = SpatialPoints(data.frame(lat,long), proj4string=CRS(proj4string(countriesSP)))  
+    # use 'over' to get indices of the Polygons object containing each point 
+    indices = over(pointsSP, countriesSP)
+    # return the ADMIN names of each country
+    indices$ADMIN  
+    #indices$ISO3 # returns the ISO3 code 
+    #indices$continent   # returns the continent (6 continent model)
+    #indices$REGION   # returns the continent (7 continent model)
+}
 
 #document
 document()
