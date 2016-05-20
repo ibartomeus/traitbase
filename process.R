71341fe0dc0499237bbdfeeb0c2da8f1738c7783
Nacho <nacho@air>
2016-05-20 22:54:46 +0200

    General cleaning

--- process.R
+++ process.R
@@ -3,44 +3,14 @@ library(roxygen2)
 library(knitr)
 library(taxize) #add to dependencies
 library(countrycode) #idem
-library(plyr) #also (not sure yet)
-library(git2r) #also (idem)
+library(plyr) #also 
+#library(git2r) #also (not sure yet)
 library(sp) #add
 library(rworldmap) #add
-library(googlesheets)
-
-#add this as func
-num.decimals <- function(x) {
-    stopifnot(class(x)=="numeric")
-    x <- sub("0+$","",x)
-    x <- sub("^.+[.]","",x)
-    nchar(x)
-}
-
-#also this one
-
-# The single argument to this function, points, is a data.frame in which:
-#   - column 1 contains the longitude in degrees
-#   - column 2 contains the latitude in degrees
-coords2country = function(lat, long){  
-    countriesSP <- getMap(resolution='low')
-    #countriesSP <- getMap(resolution='high') #you could use high res map from rworldxtra if you were concerned about detail
-    # convert our list of points to a SpatialPoints object
-    # pointsSP = SpatialPoints(points, proj4string=CRS(" +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"))
-    #setting CRS directly to that from rworldmap
-    pointsSP = SpatialPoints(data.frame(lat,long), proj4string=CRS(proj4string(countriesSP)))  
-    # use 'over' to get indices of the Polygons object containing each point 
-    indices = over(pointsSP, countriesSP)
-    # return the ADMIN names of each country
-    indices$ADMIN  
-    #indices$ISO3 # returns the ISO3 code 
-    #indices$continent   # returns the continent (6 continent model)
-    #indices$REGION   # returns the continent (7 continent model)
-}
+#library(googlesheets) # (not sure yet)
 
 #document
 document()
-
 
 #create the initial datasets
 bee_taxonomy <- data.frame(genus = NA,
@@ -58,11 +28,12 @@ bee_specimens <- data.frame(id = NA,
                         genus = NA,
                         species = NA,
                         sex = NA,
-                        trait_category = NA, #this is necesary?
+                        category = NA, #this is necesary?
                         trait = NA,
                         value = NA,
                         reference = NA,
-                        credit = NA)
+                        credit = NA,
+                        email = NA) 
 
 bee_observations <- data.frame(id = NA,
                            link_id = NA,
@@ -81,6 +52,7 @@ bee_observations <- data.frame(id = NA,
                            long = NA,
                            reference = NA,
                            credit = NA,
+                           email = NA, 
                            collector = NA,
                            taxonomist = NA)
 
@@ -99,7 +71,7 @@ bee_schema <- data.frame(category = c("metadata", "metadata",
                                "individuals", "days"),
                      test = c("c('male', 'female', 'queen')",
                               "c('mutualist', 'parastite')",
-                              "c(0:3)", "c(0:3)",
+                              "c(10)", "c(10)",
                               "c('social', 'solitary')",
                               "c('soil', 'soil_clay', 'soil_sand', 'soil_gipsy',
                                           'wood', 'cavity', 'hole', 'stem')",
@@ -115,26 +87,49 @@ bee_schema <- data.frame(category = c("metadata", "metadata",
 
 
 
-save(bee_observations, file="data/bee_observations.rda")
-save(bee_specimens, file="data/bee_specimens.rda")
+#WARNING do not run this once data is uploaded
+write.csv(bee_observations, file="data/bee_observations.csv")
+write.csv(bee_specimens, file="data/bee_specimens.scv")
 save(bee_taxonomy, file="data/bee_taxonomy.rda")
 save(bee_schema, file="data/bee_schema.rda")
-
-
-
-#issues:
-#- What to do with means and se's? Use only mean n= 1, reverse ingenieer?
-#- Bibtext?
-#- what about synomins. can we make a function that cleans and updates synonims in the master data?
-#- use travis with e.g.check data.
-#- Note:  readr::read_csv() should be used to read csv, as is faster.
 
 
 #load("data/bee_schema.rda")
 bee_schema
 
+#test functions:
 
-#to explore: 
-library(ckanr)
-?resource_update   
+dat<- data.frame( link_id = c(1,2,3),
+                     genus = c("Andrena", "Osmia", "Bombus"),
+                     species = c("cineraria", "bicornis", "terrestris"),
+                     sex = c("female", "male", "queen"),
+                     category = rep("morphological", 3), 
+                     trait = c("IT", "tongue_length", "IT"),
+                     value = c(1.2, 2.3, 3.2),
+                     reference = rep("fake",3),
+                     credit = rep("nacho", 3),
+                     email = NA)
+                    
+ 
+data2 <- data.frame(link_id = NA,
+                    genus = NA,
+                    species = NA,
+                    sex = NA,
+                    interaction_type = NA,
+                    partner_genus = NA,
+                    partner_species = NA,
+                    day = NA,
+                    month = NA,
+                    year = NA,
+                    country = NA, #Add state for US?
+                    location = NA,
+                    lat = NA,
+                    long = NA,
+                    reference = NA,
+                    credit = NA,
+                    email = NA, 
+                    collector = NA,
+                    taxonomist = NA)
 
+check_data(dat, type = "specimens")
+check_data(data2, type = "observations")
