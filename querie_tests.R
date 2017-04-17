436f947ac25d7550315b6f8c456388cb52695303
Nacho <nacho@air>
2017-04-17 23:24:05 +0200

    Add playing with queries

---
+++ querie_tests.R
@@ -0,0 +1,19 @@
+#test import
+
+
+
+#test query
+off <- resource(cnx, "species")
+
+query(off)
+query(off, conditions = buildCondition("genus", "==", "Bombus")) #fix example query species for genus.
+query(off, limit=2, skip=0)
+query(off, limit=2, skip=2)
+
+
+off <- resource(cnx, "Datasets") #qual es el nombre de la tabla??
+
+query(off)
+#query(off, conditions = buildCondition("genus", "==", "Bombus")) #fix example query species for genus.
+query(off, limit=2, skip=0)
+query(off, limit=2, skip=2)
